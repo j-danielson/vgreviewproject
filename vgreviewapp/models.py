6762f13c45f8b77b857475dc068108767a26ae2b
Josh Danielson <37751792+j-danielson@users.noreply.github.com>
2022-06-12 13:03:56 -0700

    Added videogame model and table

--- vgreviewapp/models.py
+++ vgreviewapp/models.py
@@ -1,3 +1,31 @@
+from collections import UserDict
 from django.db import models
+from pkg_resources import resource_filename
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+class Videogame(models.Model):
+    GENRE_CHOICES = [
+        ('action', 'Action'),
+        ('rpg', 'Role Playing Game'),
+        ('shooter', 'Shooter'),
+        ('sports', 'Sports'),
+        ('stealth', 'Stealth'),
+        ('rts', 'Real-Time Strategy'),
+        ('fighting', 'Fighting'),
+        ('mmo', 'Massive Multiplayer Online'),
+        ('simulation', 'Simulation'),
+        ('horror', 'Horror'),
+    ]
+    vgtitle=models.CharField(max_length=225)
+    vgdevloper=models.CharField(max_length=225)
+    vgreleasedate=models.DateField()
+    vggenre=models.CharField(max_length=25, choices=GENRE_CHOICES, default='action')
+
+    def __str__(self):
+        return self.vgtitle
+    
+    class Meta:
+        db_table='videogame'
--- vgreviewapp/views.py
+++ vgreviewapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Videogame
 
 # Create your views here.
+def index(request):
+    return render(request, 'vgreviewapp/index.html')
+
+def videogames(request):
+    videogame_list=Videogame.objects.all()
+    return render(request, 'vgreviewapp/videogames.html', {'videogame_list': videogame_list})
