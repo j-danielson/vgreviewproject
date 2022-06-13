c440234124f2f9a49a6d8714d2f590b3145932bd
Josh Danielson <37751792+j-danielson@users.noreply.github.com>
2022-06-13 12:39:02 -0700

    correcting mistake

--- vgreviewapp/models.py
+++ vgreviewapp/models.py
@@ -29,3 +29,23 @@ class Videogame(models.Model):
     
     class Meta:
         db_table='videogame'
+
+class Vgreview(models.Model):
+    RATING_CHOICES = [
+        (5, '5 Stars'),
+        (4,'4 Stars'),
+        (3,'3 Stars'),
+        (2,'2 Stars'),
+        (1,'1 Stars'),
+    ]
+    vgtitle=models.ForeignKey(Videogame, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    reviewdate=models.DateField(auto_now_add=True)
+    rating=models.IntegerField(choices=RATING_CHOICES, default=5)
+    reviewtext=models.TextField(default='enter review here')
+
+    def __str__(self):
+        return self.vgtitle
+    
+    class Meta:
+        db_table='vgreview'
